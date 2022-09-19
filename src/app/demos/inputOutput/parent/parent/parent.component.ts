f58f834879b38e11d52c8d57d9f13528f114014d
Dorian <dorian.vanhoutte1@gmail.com>
2022-09-19 10:56:21 +0200

    inputOutput

---
+++ src/app/demos/inputOutput/parent/parent/parent.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { datas } from '../datas';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html'
+})
+export class ParentComponent implements OnInit {
+
+  listPerson : { id : number, first_name : string}[] = datas
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  deleteChildById(id_person : number){
+    //alert("je veux delete l'id nb° : " + id_person)
+    let indexToDelete = this.listPerson.findIndex((person) => person.id == id_person)
+    this.listPerson.splice(indexToDelete, 1)
+  }
+
+  updateChildFirstName(updatePerson : {id : number, firstName : string}){
+    let indexToUpdate = this.listPerson.findIndex((person) => person.id == updatePerson.id)
+    this.listPerson[indexToUpdate].first_name = updatePerson.firstName
+  }
+
+
+  reverseSort(){
+    this.listPerson.reverse()
+  }
+
+}
